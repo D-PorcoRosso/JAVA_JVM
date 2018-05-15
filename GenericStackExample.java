041576aa3f4ab388accf0485dbcb0a0a50598320
shechiway <shechiway@gmail.com>
2018-05-15 11:11:59 +0800

    [item28] wildcard type extends and super example

--- GenericStackExample.java
+++ GenericStackExample.java
@@ -90,7 +90,6 @@ public class GenericStackExample {
             }
         }
 
-
         public Object pop() {
             if ( size == 0 ) 
                 throw new EmptyStackException();
@@ -110,12 +109,68 @@ public class GenericStackExample {
         }
     }
 
+    static class myStack4<E> {
+        private E[] elements;
+        private int size = 0;
+        private static final int DEFAULT_SIZE = 16;
+
+        @SuppressWarnings("unchecked")
+        myStack4() {
+            //can not create non-reifiable object
+            //elements = new E[DEFAULT_SIZE];
+            elements = (E[])new Object[DEFAULT_SIZE];
+        }
+
+        public void push( E e ) {
+            ensureCapacity();
+            elements[size++] = e;
+        }
+
+        public void pushAll(Iterable<? extends E> src) {
+            for (E e : src ) {
+                push(e);
+            }
+        }
+
+        public E pop() {
+            if ( size == 0 ) 
+                throw new EmptyStackException();
+            E result = elements[--size];
+            elements[size] = null;
+            return result;
+        }
+
+        public void popAll(Collection<? super E> dst) {
+            while(!isEmpty())
+                dst.add(pop());
+        }
+
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        private void ensureCapacity() {
+            if (elements.length == size)
+                elements = Arrays.copyOf(elements, 2 * size +1);
+        }
+    }
+
     public static void main(String[] args) {
         //compile error GenericStackExample.java:116: error: incompatible types: Iterable<Integer> cannot be converted to Iterable<Number>
         //numberStack.pushAll(integers);
         /*myStack3<Number> numberStack = new myStack3<>();
         Iterable<Integer> integers = {1,3,4};
         numberStack.pushAll(integers);*/
+        myStack4<Number> numberStack = new myStack4<>();
+        Integer[] temp = {1,2,3,4,5};
+        Iterable<Integer> integers =  Arrays.asList(temp);
+        numberStack.pushAll(integers);
+
+        Collection<Object> collection = new ArrayList<>();
+        numberStack.popAll(collection);
+        for ( Object o : collection ) {
+            System.out.println(o);
+        }
     }
 
 }
