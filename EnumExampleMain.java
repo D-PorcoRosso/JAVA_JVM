dae37f26b28989e88d4395ebfbd73ebfe1a6d0ec
shechiway <shechiway@gmail.com>
2018-05-24 11:32:28 +0800

    [item32] EnumSet usage example

--- EnumExampleMain.java
+++ EnumExampleMain.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class EnumExampleMain {
     public static void main(String[] args) {
         for ( EnumExample temp : EnumExample.values() )
@@ -10,5 +12,14 @@ public class EnumExampleMain {
         }
 
         System.out.println(PayrollDay.FRIDAY.pay(8, 1.5));
+
+        EnumSet<EnumExample> s = EnumSet.of(EnumExample.C,EnumExample.A);
+        for ( EnumExample a : s ) {
+            System.out.println(a);
+        }
+        s = EnumSet.range(EnumExample.A,EnumExample.C);
+        for ( EnumExample a : s ) {
+            System.out.println(a);
+        }
     }
 }
